49da4286544a479d138f046f958ef271830ecdb6
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-05 05:37:31 +0300

    feat: new abstractions added

---
+++ ReportsLibrary/Tasks/ITask.cs
@@ -0,0 +1,16 @@
+using ReportsLibrary.Employees;
+using ReportsLibrary.Tasks.TaskStates;
+
+namespace ReportsLibrary.Tasks
+{
+    public interface ITask
+    {
+        public void ChangeContent(Employee changer, string newContent);
+        public void AddComment(Employee changer, string comment);
+        public void ChangeImplementer(Employee changer, Employee newImplementer);
+        public void ChangeState(Employee changer, TaskState newState);
+        public void MakeSnapshot();
+        public void RestorePreviousSnapshot();
+        public void RestoreNextSnapshot();
+    }
+}
