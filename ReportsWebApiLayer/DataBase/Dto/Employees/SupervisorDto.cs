951dd16866ef461e40551fcd7c9c21c47ec1663f
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-08 05:29:05 +0300

    fix: DTOs removed

--- ReportsWebApiLayer/DataBase/Dto/Employees/SupervisorDto.cs
+++
@@ -1,7 +0,0 @@
-using ReportsLibrary.Employees;
-
-namespace ReportsWebApiLayer.DataBase.Dto.Employees;
-
-public class SupervisorDto : EmployeeDto
-{
-}
--- ReportsWebApiLayer/DataBase/Dto/Employees/TeamLeadDto.cs
+++
@@ -1,8 +0,0 @@
-using ReportsLibrary.Entities;
-
-namespace ReportsWebApiLayer.DataBase.Dto.Employees;
-
-public class TeamLeadDto : EmployeeDto
-{
-    public List<WorkTeam> WorkTeams { get; set; }
-}
