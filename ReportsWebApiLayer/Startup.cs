4e2eac5a603113673ecf79109ad8b5ffd928d352
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-10 05:53:09 +0300

    fix: Startup class updated

--- ReportsWebApiLayer/Startup.cs
+++ ReportsWebApiLayer/Startup.cs
@@ -1,7 +1,5 @@
-using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
-using ReportsLibrary.Employees;
 using ReportsWebApiLayer.DataBase;
 using ReportsWebApiLayer.Services;
 using ReportsWebApiLayer.Services.Interfaces;
@@ -10,25 +8,23 @@ namespace ReportsWebApiLayer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
             services.AddDbContext<ReportsDbContext>(opt =>
                 opt.UseSqlite("Data Source=Reports.db;Cache=Shared;"));
 
-            services.AddSwaggerGen(c => 
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "WebApiReports", Version = "v1"}));
+            services.AddSwaggerGen(c =>
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApiReports", Version = "v1" }));
 
-            // уничтожаются после окончания запросов
+            // removing after completion of requests
             services.AddScoped<IEmployeeService, EmployeeService>();
             services.AddScoped<ITaskService, TaskService>();
         }
@@ -39,7 +35,6 @@ namespace ReportsWebApiLayer
             // var config = new MapperConfiguration(cfg => {
             //     cfg.CreateMap<Employee, Employee>();
             // });
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
