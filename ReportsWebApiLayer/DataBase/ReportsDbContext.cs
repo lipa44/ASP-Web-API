6f8f9d6c122042849fe75765c3001be3ddbcd92a
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-10 05:51:29 +0300

    feat: Fluent API for tables added

--- ReportsWebApiLayer/DataBase/ReportsDbContext.cs
+++ ReportsWebApiLayer/DataBase/ReportsDbContext.cs
@@ -1,8 +1,9 @@
+using Microsoft.AspNetCore.Routing.Template;
 using Microsoft.EntityFrameworkCore;
 using ReportsLibrary.Employees;
 using ReportsLibrary.Entities;
 using ReportsLibrary.Tasks;
-using ReportsLibrary.Tasks.TaskStates;
+using ReportsLibrary.Tasks.TaskSnapshots;
 using Task = ReportsLibrary.Tasks.Task;
 
 namespace ReportsWebApiLayer.DataBase;
@@ -12,28 +13,61 @@ public class ReportsDbContext : DbContext
     public ReportsDbContext(DbContextOptions<ReportsDbContext> options)
         : base(options)
     {
-        Database.EnsureCreated();
+        // Database.EnsureDeleted();
+        Database.EnsureCreatedAsync();
     }
-    
+
     public DbSet<Task> Tasks { get; set; }
     public DbSet<Employee> Employees { get; set; }
     public DbSet<WorkTeam> WorkTeams { get; set; }
 
+    // using Fluent API
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        // modelBuilder.Entity<Task>().ToTable("Tasks")
-        //     .HasOne<Employee>()
-        //     .WithMany()
-        //     .HasForeignKey(p => p.Id)
-        //     .OnDelete(DeleteBehavior.Cascade);;
-        //
-        // modelBuilder.Entity<WorkTeam>().ToTable("WorkTeam")
-        //     .HasMany<Employee>();
-    
+        modelBuilder.Entity<Employee>()
+            .HasMany<Task>()
+            .WithOne(t => t.Owner)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Task>()
+            .HasOne(t => t.Owner)
+            .WithMany()
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<Task>()
+            .HasMany(t => t.Comments)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Task>()
+            .HasMany(t => t.Modifications)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Sprint>()
+            .HasMany(s => s.Tasks)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<WorkTeam>()
+            .HasOne(t => t.TeamLead)
+            .WithMany()
+            .OnDelete(DeleteBehavior.SetNull);
+
+        modelBuilder.Entity<TaskModification>()
+            .HasOne(t => t.Changer)
+            .WithMany()
+            .OnDelete(DeleteBehavior.NoAction);
+
+        modelBuilder.Entity<TaskComment>()
+            .HasOne(t => t.Commentator)
+            .WithMany()
+            .OnDelete(DeleteBehavior.NoAction);
+
         base.OnModelCreating(modelBuilder);
     }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         // modelBuilder.Entity<Task>().ToTable("Tasks")
         //     .HasOne<Employee>()
