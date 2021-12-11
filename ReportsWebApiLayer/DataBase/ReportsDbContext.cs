cdc60c92b55e9c3027ab15ffe033cfcc585b3814
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-11 04:50:05 +0300

    fix: relationships between db entities edited

--- ReportsWebApiLayer/DataBase/ReportsDbContext.cs
+++ ReportsWebApiLayer/DataBase/ReportsDbContext.cs
@@ -23,53 +23,57 @@ public class ReportsDbContext : DbContext
     // using Fluent API
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // One-to-many: many employees refers to one chief
+        modelBuilder.Entity<Employee>()
+            .HasMany(e => e.Subordinates)
+            .WithOne(e => e.Chief)
+            .OnDelete(DeleteBehavior.SetNull);
+
+        // One-to-many: many tasks refers to one owner
         modelBuilder.Entity<Task>()
             .HasOne(t => t.Owner)
-            .WithMany()
+            .WithMany(e => e.Tasks)
             .OnDelete(DeleteBehavior.SetNull);
 
-        modelBuilder.Entity<Employee>()
-            .HasMany(t => t.Subordinates)
-            .WithOne()
-            .OnDelete(DeleteBehavior.SetNull);
-
-        modelBuilder.Entity<Employee>()
-            .HasOne(e => e.Chief)
-            .WithMany()
-            .OnDelete(DeleteBehavior.SetNull);
-
-        modelBuilder.Entity<Employee>()
-            .Property(e => e.Tasks)
-            .HasConversion(
-                v => JsonConvert.SerializeObject(v),
-                v => JsonConvert.DeserializeObject<List<Task>>(v));
-
+        // One-to-many: many comments has refers to one task
         modelBuilder.Entity<Task>()
             .HasMany(t => t.Comments)
             .WithOne()
             .OnDelete(DeleteBehavior.Cascade);
 
-        modelBuilder.Entity<Task>()
-            .Property(e => e.Modifications)
-            .HasConversion(
-                v => JsonConvert.SerializeObject(v),
-                v => JsonConvert.DeserializeObject<List<TaskModification>>(v));
-
+        // One-to-many: many tasks refers to one sprint
         modelBuilder.Entity<Sprint>()
             .HasMany(s => s.Tasks)
             .WithOne()
             .OnDelete(DeleteBehavior.Cascade);
 
+        // One-to-many: many work teams refers to one employee
         modelBuilder.Entity<WorkTeam>()
             .HasOne(t => t.TeamLead)
             .WithMany()
             .OnDelete(DeleteBehavior.SetNull);
 
+        // One-to-many: many comments refers to one employee
         modelBuilder.Entity<TaskComment>()
             .HasOne(t => t.Commentator)
             .WithMany()
             .OnDelete(DeleteBehavior.NoAction);
 
+        /* SERIALIZATION (OPTIONAL) */
+
+        // Set employee's tasks serialization
+        // modelBuilder.Entity<Employee>()
+        //     .Property(e => e.Tasks)
+        //     .HasConversion(
+        //         v => JsonConvert.SerializeObject(v),
+        //         v => JsonConvert.DeserializeObject<List<Task>>(v));
+        //
+        // // Set task's modifications serialization
+        // modelBuilder.Entity<Task>()
+        //     .Property(e => e.Modifications)
+        //     .HasConversion(
+        //         v => JsonConvert.SerializeObject(v),
+        //         v => JsonConvert.DeserializeObject<List<TaskModification>>(v));
         base.OnModelCreating(modelBuilder);
     }
 
