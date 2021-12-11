18e46629c6bbfd72d5f60ba100fc7a6578e7e6ad
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-11 03:34:54 +0300

    fix: temporary public lists added

--- ReportsLibrary/Employees/Employee.cs
+++ ReportsLibrary/Employees/Employee.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using ReportsLibrary.Tasks;
 using ReportsLibrary.Tools;
 
 namespace ReportsLibrary.Employees
@@ -27,9 +28,9 @@ namespace ReportsLibrary.Employees
         public string Surname { get; init; }
         public Guid Id { get; init; }
         public EmployeeRoles Role { get; init; }
-        public IReadOnlyCollection<Employee> Subordinates => Employees;
         public Employee Chief { get; protected set; }
-        protected List<Employee> Employees { get; init; } = new ();
+        public List<Employee> Subordinates { get; init; } = new ();
+        public List<Task> Tasks { get; init; } = new ();
 
         public void SetChief(Employee chief)
         {
@@ -45,16 +46,33 @@ namespace ReportsLibrary.Employees
             if (IsSubordinateExist(subordinate))
                 throw new ReportsException($"Employee {subordinate} already exists in {this}'s subordinates");
 
-            subordinate.SetChief(this);
-            Employees.Add(subordinate);
+            Subordinates.Add(subordinate);
         }
 
         public void RemoveSubordinate(Employee subordinate)
         {
             ArgumentNullException.ThrowIfNull(subordinate);
 
-            if (!Employees.Remove(subordinate))
+            if (!Subordinates.Remove(subordinate))
                 throw new ReportsException($"Employee {subordinate} doesn't exist in {this}'s subordinates");
+        }
+
+        public void AddTask(Task task)
+        {
+            ArgumentNullException.ThrowIfNull(task);
+
+            if (IsTaskExist(task))
+                throw new ReportsException($"Task {task} already exists in {this}'s tasks");
+
+            Tasks.Add(task);
+        }
+
+        public void RemoveTask(Task task)
+        {
+            ArgumentNullException.ThrowIfNull(task);
+
+            if (!Tasks.Remove(task))
+                throw new ReportsException($"Task {task} doesn't exist in {this}'s tasks");
         }
 
         public override string ToString() => $"{Name} {Surname}";
@@ -62,7 +80,8 @@ namespace ReportsLibrary.Employees
         public override bool Equals(object obj) => Equals(obj as Employee);
         public override int GetHashCode() => HashCode.Combine(Id, Name, Surname);
 
-        protected bool IsSubordinateExist(Employee employee) => Employees.Any(e => e.Equals(employee));
+        protected bool IsSubordinateExist(Employee employee) => Subordinates.Any(e => e.Equals(employee));
+        protected bool IsTaskExist(Task task) => Tasks.Any(t => t.Equals(task));
 
         private bool Equals(Employee employee) => employee is not null && employee.Id == Id
                                                                         && employee.Name == Name
