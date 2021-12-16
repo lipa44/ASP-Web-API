3b9eab85516fc48ed76441993cef1d6c2d41f035
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-16 21:00:36 +0300

    feat: TaskStates && TaskChangeCommands enum's added

---
+++ ReportsLibrary/Tools/TaskChangeCommands.cs
@@ -0,0 +1,10 @@
+namespace ReportsLibrary.Tools;
+
+public enum TaskChangeCommands
+{
+    SetTitle = 1,
+    SetContent,
+    SetState,
+    SetOwner,
+    AddComment,
+}
---
+++ ReportsLibrary/Tools/TaskStates.cs
@@ -0,0 +1,8 @@
+namespace ReportsLibrary.Tools;
+
+public enum TaskStates
+{
+    Open = 1,
+    Active,
+    Resolved,
+}
