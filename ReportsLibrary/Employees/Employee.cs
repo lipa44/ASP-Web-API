a5aeea6a3a2e8c01ece0f4c8e54b8ffda6a02d16
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-17 01:51:17 +0300

    fix: subordinates list && report added to employee

--- ReportsLibrary/Employees/Employee.cs
+++ ReportsLibrary/Employees/Employee.cs
@@ -10,6 +10,7 @@ namespace ReportsLibrary.Employees
     public class Employee
     {
         private readonly List<ReportsTask> _tasks = new ();
+        private readonly List<Employee> _subordinates = new ();
 
         public Employee() { }
 
@@ -25,6 +26,8 @@ namespace ReportsLibrary.Employees
             Surname = surname;
             Id = id;
             Role = role;
+            Report = new (this);
+            ReportId = Report.Id;
         }
 
         public string Name { get; init; }
@@ -33,11 +36,12 @@ namespace ReportsLibrary.Employees
         public EmployeeRoles Role { get; init; }
         public Employee Chief { get; protected set; }
         public Guid? ChiefId { get; protected set; }
-
         public WorkTeam WorkTeam { get; protected set; }
         public Guid? WorkTeamId { get; protected set; }
+        public Report Report { get; init; }
+        public Guid? ReportId { get; init; }
 
-        // public List<Employee> Subordinates { get; init; } = new ();
+        public IReadOnlyCollection<Employee> Subordinates => _subordinates;
         public IReadOnlyCollection<ReportsTask> Tasks => _tasks;
         public void SetChief(Employee chief)
         {
@@ -47,23 +51,26 @@ namespace ReportsLibrary.Employees
             ChiefId = chief.Id;
         }
 
-        // public void AddSubordinate(Employee subordinate)
-        // {
-        //     ArgumentNullException.ThrowIfNull(subordinate);
-        //
-        //     if (IsSubordinateExist(subordinate))
-        //         throw new ReportsException($"Employee {subordinate} already exists in {this}'s subordinates");
-        //
-        //     Subordinates.Add(subordinate);
-        // }
-        //
-        // public void RemoveSubordinate(Employee subordinate)
-        // {
-        //     ArgumentNullException.ThrowIfNull(subordinate);
-        //
-        //     if (!Subordinates.Remove(subordinate))
-        //         throw new ReportsException($"Employee {subordinate} doesn't exist in {this}'s subordinates");
-        // }
+        public Report CommitChangesToReport() => Report.CommitChanges(_tasks);
+
+        public void AddSubordinate(Employee subordinate)
+        {
+            ArgumentNullException.ThrowIfNull(subordinate);
+
+            if (IsSubordinateExist(subordinate))
+                throw new ReportsException($"Employee {subordinate} already exists in {this}'s subordinates");
+
+            _subordinates.Add(subordinate);
+        }
+
+        public void RemoveSubordinate(Employee subordinate)
+        {
+            ArgumentNullException.ThrowIfNull(subordinate);
+
+            if (!_subordinates.Remove(subordinate))
+                throw new ReportsException($"Employee {subordinate} doesn't exist in {this}'s subordinates");
+        }
+
         public void AddTask(ReportsTask reportsTask)
         {
             ArgumentNullException.ThrowIfNull(reportsTask);
@@ -82,13 +89,36 @@ namespace ReportsLibrary.Employees
                 throw new ReportsException($"Task {reportsTask} doesn't exist in {this}'s tasks");
         }
 
+        public void SetWorkTeam(WorkTeam workTeam)
+        {
+            ArgumentNullException.ThrowIfNull(workTeam);
+
+            if (IsWorkTeamSet(workTeam))
+                throw new ReportsException($"{this} already in {workTeam}");
+
+            WorkTeam = workTeam;
+            WorkTeamId = workTeam.Id;
+        }
+
+        public void RemoveWorkTeam(WorkTeam workTeam)
+        {
+            ArgumentNullException.ThrowIfNull(workTeam);
+
+            if (!IsWorkTeamSet(workTeam))
+                throw new ReportsException($"{this} doesn't exist in any work team to remove team");
+
+            WorkTeam = null;
+            WorkTeamId = default;
+        }
+
         public override string ToString() => $"{Name} {Surname}";
 
         public override bool Equals(object obj) => Equals(obj as Employee);
         public override int GetHashCode() => HashCode.Combine(Id, Name, Surname);
 
-        // protected bool IsSubordinateExist(Employee employee) => Subordinates.Any(e => e.Equals(employee));
+        protected bool IsSubordinateExist(Employee employee) => Subordinates.Any(e => e.Equals(employee));
         protected bool IsTaskExist(ReportsTask reportsTask) => Tasks.Any(t => t.Equals(reportsTask));
+        protected bool IsWorkTeamSet(WorkTeam workTeam) => WorkTeamId.Equals(workTeam.Id);
         private bool Equals(Employee employee) => employee is not null && employee.Id == Id
                                                                        && employee.Name == Name
                                                                        && employee.Surname == Surname;
