df7ec851ad01f11e9183df0821c77f8ea76cd344
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-18 23:32:20 +0300

    chore: enums moved to their own folder

--- ReportsLibrary/Enums/TaskChangeCommands.cs
+++ ReportsLibrary/Enums/TaskChangeCommands.cs
@@ -1,4 +1,4 @@
-namespace ReportsLibrary.Tools;
+namespace ReportsLibrary.Enums;
 
 public enum TaskChangeCommands
 {
---
+++ ReportsLibrary/Enums/TaskModificationActions.cs
@@ -0,0 +1,9 @@
+namespace ReportsLibrary.Enums;
+
+public enum TaskModificationActions
+{
+    ContentChanged = 1,
+    CommentAdded,
+    ImplementerChanged,
+    StateChanged,
+}
