6e1a6e5fc201746ac7de57fe065ae1050b08eb74
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-19 01:09:47 +0300

    feat: Report entity's IsReportDone property added

--- ReportsLibrary/Entities/Report.cs
+++ ReportsLibrary/Entities/Report.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using ReportsLibrary.Employees;
 using ReportsLibrary.Tasks;
+using ReportsLibrary.Tools;
 
 namespace ReportsLibrary.Entities;
 
@@ -12,38 +13,36 @@ public class Report
 
     public Report() { }
 
-    public Report(Employee owner)
-    {
-        ArgumentNullException.ThrowIfNull(owner);
-        Owner = owner;
-    }
+    public Report(Employee owner) =>
+        Owner = owner ?? throw new ReportsException("Owner to set into report is null");
 
     public Employee Owner { get; init; }
     public Guid OwnerId { get; init; }
-    public WorkTeam WorkTeam { get; private set; }
     public Guid Id { get; init; } = Guid.NewGuid();
+    public bool IsReportDone { get; private set; }
     public IReadOnlyCollection<TaskModification> Modifications => _modifications;
 
     public Report CommitModifications(ICollection<TaskModification> modificationsToCommit)
     {
         ArgumentNullException.ThrowIfNull(modificationsToCommit);
 
+        if (IsReportDone)
+            throw new PermissionDeniedException($"Can't commit into report {Id}, because it's marked as done");
+
         _modifications.AddRange(GetUncommittedModifications(modificationsToCommit));
 
         return this;
     }
 
-    // public void SetWorkTeam(WorkTeam workTeam)
-    // {
-    //     ArgumentNullException.ThrowIfNull(workTeam);
-    //
-    //     WorkTeam = workTeam;
-    //     WorkTeamId = workTeam.Id;
-    // }
-    private List<TaskModification> GetUncommittedModifications(ICollection<ReportsTask> tasksToCommit) =>
-        tasksToCommit.SelectMany(t => t.Modifications)
-            .Except(_modifications
-                .Select(m => m)).ToList();
+    public void SetReportAsDone(Employee changer)
+    {
+        ArgumentNullException.ThrowIfNull(changer);
+
+        if (changer.Id != OwnerId)
+            throw new PermissionDeniedException($"{changer} has not permission to set report {Id} as done");
+
+        IsReportDone = true;
+    }
 
     private List<TaskModification> GetUncommittedModifications(ICollection<TaskModification> modificationsToCommit) =>
         modificationsToCommit
