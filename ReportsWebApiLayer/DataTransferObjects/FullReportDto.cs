364650f17f44ff6c149ff8ba751cacfe898f0bcd
Mikhail Libchenko <libchenkomikhail@gmail.com>
2021-12-20 02:13:29 +0300

    feat: some ReportDto's attributes moved into FullReportDto

--- ReportsWebApiLayer/DataTransferObjects/FullReportDto.cs
+++ ReportsWebApiLayer/DataTransferObjects/FullReportDto.cs
@@ -1,7 +1,10 @@
+using ReportsLibrary.Tasks;
+
 namespace ReportsWebApiLayer.DataTransferObjects;
 
-public class ReportDto
+public class FullReportDto
 {
     public string OwnerData { get; init; }
     public string WorkTeamName { get; init; }
+    public IReadOnlyCollection<TaskModification> Modifications { get; init; }
 }
